12a74c3560ddfe6a6768c3b3252b04cfa092252f
Sergii Maksymov <ua.maximoff@gmail.com>
2025-05-23 22:18:50 +0200

    flake: update flake deps

--- flake.nix
+++ flake.nix
@@ -13,9 +13,7 @@
       ];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
-        overlayAttrs = {
-            inherit (config.packages) zed-editor;
-        };
+        overlayAttrs = config.packages;
         packages = rec {
             zed-editor = pkgs.callPackage ./package {};
             default = zed-editor;
