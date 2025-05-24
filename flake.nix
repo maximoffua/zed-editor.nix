3c7d2b04e94235eb8dcc6844fcfdd047294ef622
Sergii Maksymov <ua.maximoff@gmail.com>
2025-05-24 03:41:34 +0200

    add preview version

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ flake-parts, ... }:
@@ -12,19 +14,28 @@
         inputs.flake-parts.flakeModules.easyOverlay
       ];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
+      perSystem = { config, self', inputs', pkgs, system, ... }: let
+        rustPackages = inputs'.fenix.packages;
+        rustWasm = rustPackages.combine [
+          rustPackages.targets.wasm32-wasip1.latest.rust-std
+          rustPackages.latest.cargo
+          rustPackages.latest.rustc
+        ];
+      in {
         overlayAttrs = config.packages;
         packages = rec {
+          default = zed-editor;
+          zed-editor = pkgs.callPackage ./package {};
           zed-editor-bin = pkgs.callPackage ./package/binary.nix {};
           zed-editor-fhs = zed-editor.fhs;
-          zed-editor = pkgs.callPackage ./package {};
-          default = zed-editor;
+          zed-editor-dev = zed-editor.fhsWithPackages (_: [rustWasm pkgs.node]);
+          zed-editor-pre = pkgs.callPackage ./package/pre-release.nix {
+            inherit rustWasm;
+            supportCustomExtensions = true;
+          };
         };
       };
       flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
       };
     };
 }
