57c446a1f608328248ec89d10ebd4ff56f49ffda
Sergii Maksymov <ua.maximoff@gmail.com>
2025-06-05 23:54:12 +0200

    zed-editor: 0.188.5 -> 0.189.5
    Diff: https://github.com/zed-industries/zed/compare/refs/tags/v0.188.5...v0.189.5
    
    Changelog: https://github.com/zed-industries/zed/releases/tag/v0.189.5

--- package/default.nix
+++ package/default.nix
@@ -97,7 +97,7 @@ assert withGLES -> stdenv.hostPlatform.isLinux; let
 in
   rustPlatform.buildRustPackage (finalAttrs: {
     pname = "zed-editor";
-    version = "0.188.5";
+    version = "0.189.5";
 
     outputs =
       ["out"]
@@ -109,7 +109,7 @@ in
       owner = "zed-industries";
       repo = "zed";
       tag = "v${finalAttrs.version}";
-      hash = "sha256-3xLgP9ZcAl7ELLbP12R/5Bm4BcfhvDvSo6v1PdSK+rc=";
+      hash = "sha256-d1d3WgUVamrYWVosljQiEPZGNNDldtM1YwZhxseX4+w=";
     };
 
     patches = [
@@ -134,7 +134,7 @@ in
       '';
 
     useFetchCargoVendor = true;
-    cargoHash = "sha256-WiOyw6/o1W8OX1o6ja9CAo410+Sc56gCYCY6veLnxjE=";
+    cargoHash = "sha256-YhdwCNTbBphWugguoWQqrGf2fRB5Jv40MElW6hbcxtk=";
 
     nativeBuildInputs =
       [
