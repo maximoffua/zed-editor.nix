62ccb8e05f65c9ff736383220bb8dfc49686939f
Sergii Maksymov <ua.maximoff@gmail.com>
2025-07-17 12:02:28 +0200

    zed-editor-bin: 0.194.3 -> 0.195.3
    Diff: https://github.com/zed-industries/zed/compare/v0.194.3...v0.195.3
    
    Changelog: https://github.com/zed-industries/zed/releases/tag/v0.195.3

--- package/binary.nix
+++ package/binary.nix
@@ -18,7 +18,7 @@
   testers,
   lib,
 }: let
-  version = "0.194.3";
+  version = "0.195.3";
 
   # Map from Nix system → { url, sha256, type }
   assets = {
@@ -26,7 +26,7 @@
       url =
         "https://github.com/zed-industries/zed/releases/download/"
         + "v${version}/zed-linux-x86_64.tar.gz";
-      sha256 = "sha256-RAmTwixylhBM0XpbC4Ps0QKW+urUt07rL9YeKpsRLuQ=";
+      sha256 = "sha256-mKqtBakaoi6f2Ec9jSqQx8uZJ9SGrmbrwsxQHBeOR/E=";
       type = "tar.gz";
     };
     "aarch64-linux" = {
